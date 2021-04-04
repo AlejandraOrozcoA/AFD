5c9b4c9d113c6b1f0ab7514684f25a506b287447
Ale <aorozcoa1400@alumno.ipn.mx>
2021-04-04 17:21:45 -0500

    identifica transiciones de cada estado

--- src/afd/AFD.java
+++ src/afd/AFD.java
@@ -1,19 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package afd;
 
-/**
- *
- * @author Ale
- */
 public class AFD {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         TablaTrans tbl = new TablaTrans("src\\afd\\resources\\Pruebas.txt");
     }
--- src/afd/TablaTrans.java
+++ src/afd/TablaTrans.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package afd;
 
 import java.io.BufferedReader;
@@ -10,15 +6,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 
-/**
- *
- * @author Ale
- */
 public class TablaTrans {
     String dir;
     ArrayList <String> estados = new ArrayList <String> ();
     ArrayList <String> alfabeto  = new ArrayList <String> ();
-    ArrayList trans = new ArrayList ();
+    ArrayList <ArrayList> trans = new ArrayList <ArrayList> ();
     String edoAcep; 
     FileReader fr;
 
@@ -33,27 +25,39 @@ public class TablaTrans {
             File archivo = new File(dir);
             fr = new FileReader(archivo);
             BufferedReader br = new BufferedReader (fr);
-            
             String linea;
+            //Recorre el archivo
             int aux = 0;
+           
             while((linea = br.readLine()) != null){
+                ArrayList <String>  tr = new ArrayList <String> ();
+                //Guarda el Alfabeto 
                 if (aux == 0) {
                     String ar []= linea.split(",");
                     for (int i = 2; i < ar.length ; i++) {
                         alfabeto.add(ar[i]); 
                     }
                 }
+                //Guarda los estados
                 else if(aux > 0){
                     String ar []= linea.split(",");
                     estados.add(ar[1]);
-                    System.out.println(ar[0]);
+                    //Guarda el estado de aceptacion
                     if(ar[0].equals("*")){
                         edoAcep = ar[1];
                     }
+                    //Identifica las tansiciones de cada estado
+                    for (int i = 2; i < ar.length ; i++) {
+                        tr.add(ar[i]);
+                        
+                    }
+                    trans.add(tr);
+                    //tr.clear();
                 }
                 
                 ++aux;
             }
+            imprimirTrans();
         }
         catch (Exception e) {
             System.out.println("No se puedo leer el archivo");
@@ -64,8 +68,17 @@ public class TablaTrans {
                     fr.close();
                 }
             } catch (Exception e) {
-                e.printStackTrace();
+                System.out.println("Error al cerrar el archivo");
             }
         }
     }    
+
+    private void imprimirTrans() {
+        for (int i = 0; i < estados.size() ; i++) {
+            System.out.println("Estado: "+ estados.get(i));
+            for (int j = 0; j < trans.size() ; j++) {
+                System.out.println("Si: "+alfabeto.get(j)+" pasa a: "+ trans.get(i).get(j));
+            }
+        }
+    }
 }
